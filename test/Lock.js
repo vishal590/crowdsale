8942ec61b0f11c9a3a3eedc1b67b8deca2aaf5ea
curious <vishal238128@gmail.com>
2022-11-28 19:08:14 +0530

    add code in lock.js

--- test/Lock.js
+++ test/Lock.js
@@ -8,6 +8,14 @@ describe("Artwork smart contract tests", function(){
   })
 
   it("NFT is minted successfully", async function(){
+    [account1] = await ethers.getSigners();
+
+    expect(await artwork.balanceOf(account1.address)).to.equal(0);
+
+    const tokenURI = "https://opensea-creatures-api.herokuapp.com/api/creature/1";
+    const tx = await artwork.connect(account1).mint(tokenURI);
+
+    expect(await artwork.balanceOf(account1.address)).to.equal(1);
 
   })
 
