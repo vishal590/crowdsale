107917d929e31d4c274685168032f75f357b2f8c
curious <vishal238128@gmail.com>
2022-11-29 19:40:55 +0530

    code added

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,43 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-chai-matchers");
+require("@nomiclabs/hardhat-etherscan");
+require("@nomiclabs/hardhat-ethers");
+require("dotenv").config();
+
+task("accounts","Prints the list of accounts", async(taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+  for(const account of accounts){
+    console.log(account.address);
+  }
+});
+
+task("deploy", "Deploy the smart contracts", async(taskArgs, hre) => {
+  const Artwork = await hre.ethers.getContractFactory("Artwork");
+  const artwork = await Artwork.deploy("Artwork Contract", "ART");
+
+  await artwork.deployed();
+
+  await hre.run("verify:verify", {
+    address: artwork.address,
+    constructorArguments: [
+      "Artwork Contract",
+      "ART"
+    ]
+  })
+});
+
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    mumbai: {
+      url: "https://matic-testnet-archive-rpc.bwarelabs.com",
+      accounts: [
+        process.env.PRIVATE_KEY,
+      ]
+    }
+  },
+  etherscan: {
+    apiKey: process.env.POLYGONSCAN_KEY,
+  }
 };
